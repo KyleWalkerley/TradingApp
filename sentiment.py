30af0802ef3dc4e08e4195f9e993d224a4d30da0
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2025-07-02 20:43:56 +0200

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -2,7 +2,7 @@ import requests
 from textblob import TextBlob
 
 # Optional: Get your own key at https://newsapi.org
-NEWS_API_KEY = "f3bc50b5708744aca5cd6c4e8e7ae382"  # Replace with your real key if needed
+NEWS_API_KEY = "XXXXX"  # Replace with your real key if needed
 
 def get_news_headlines(ticker):
     url = f"https://newsapi.org/v2/everything?q={ticker}&sortBy=publishedAt&apiKey={NEWS_API_KEY}&language=en"
